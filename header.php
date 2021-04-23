077467683a0af3a36fb450b43ca48140c6205635
Jonathas Veras <Jonathasveras42@gmail.com>
2021-04-23 19:14:07 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Introduções</title>
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+</head>
+<body>
+    <header>
+        <div class="container-fluid">
+            <h1 class="h1"style="color: white;">Estudando PHP</h1>
+        </div>
+</header>
