b90f385ee349900709b7443c73ee11206021f651
Jonathas Veras <Jonathasveras42@gmail.com>
2021-04-23 19:14:03 -0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+    <footer>
+        <script src="public/js/jquery.js"></script>
+        <script src="public/js/bootstrap.bundle.min.js"></script>
+        <script src="script.js"></script>
+    </footer>
+</body>
+</html>
